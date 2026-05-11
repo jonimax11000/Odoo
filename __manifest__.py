1d646b3caf97db0701d9fc364f5a82c08ec514a3
jonimax11000 <jonimax11000@gmail.com>
2026-05-11 10:58:05 +0200

    Initial commit: basic Odoo 19 module structure for cookast_weather

---
+++ __manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Cookast Weather',
+    'version': '19.0.1.0.0',
+    'category': 'Extra Tools',
+    'summary': 'Weather integration for Cookast',
+    'description': """
+        Weather integration for Cookast
+    """,
+    'author': 'Joni',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': False,
+    'license': 'LGPL-3',
+}
